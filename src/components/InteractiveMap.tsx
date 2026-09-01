5fff7efaae6f4d59190d5a6ea42bbe32ae8d8dce
revamingle <revamingle@gmail.com>
2026-08-31 20:20:13 -0700

    Update TileLayer attribution and URL for map

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -230,8 +230,10 @@ export function InteractiveMap({ dataPoints }: InteractiveMapProps) {
           zoomControl={false}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          <TileLayer
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+/>
           />
           <MapUpdater center={center} zoom={zoom} />
           
