90dd94af080f4648c70aed893fae1a62ab9d07f1
revamingle <revamingle@gmail.com>
2026-08-31 20:32:54 -0700

    fix(map): resolve duplicate TileLayer component

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -230,10 +230,8 @@ export function InteractiveMap({ dataPoints }: InteractiveMapProps) {
           zoomControl={false}
         >
           <TileLayer
-          <TileLayer
-  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-/>
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
           />
           <MapUpdater center={center} zoom={zoom} />
           
